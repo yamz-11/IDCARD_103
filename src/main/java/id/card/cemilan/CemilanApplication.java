c5c997541a49aa749a955c669de254f0e75743a4
yamz-11 <wiratamajrz@gmail.com>
2022-11-14 15:41:35 +0700

    membuat project id card spring boot

---
+++ src/main/java/id/card/cemilan/CemilanApplication.java
@@ -0,0 +1,13 @@
+package id.card.cemilan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemilanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemilanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/card/cemilan/CemilanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.card.cemilan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemilanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
